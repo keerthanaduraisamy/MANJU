e3514e7542561b70b0ca053ac2a94bde20fd1b4e
keerthanaduraisamy <keerthana09297@gmail.com>
2017-07-06 10:45:31 +0530

    Create Natural.java

---
+++ Natural.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.io.*;
+public class Natural{
+  
+  public static void main(String args[])
+  {
+    
+    Scanner sc=new Scanner(System.in);
+    int n,i=1,sum=0;
+    System.out.println("enter the number");
+    n=sc.nextInt();
+    while(i<=n)
+    {
+      
+      sum=sum+i;
+      i++;
+      }
+      System.out.println("the sum is "+sum);
+      }
+      }
