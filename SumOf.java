fc71a783612319b1c5c54b3736576c813ae6ffb6
keerthanaduraisamy <keerthana09297@gmail.com>
2017-07-06 14:46:03 +0530

    Create SumOf.java

---
+++ SumOf.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.io.*;
+public class SumOf{
+  
+  public static void main(String args[])
+  {
+    
+    int n,i,sum=0;
+    Scanner sc=new Scanner(System.in);
+    System.out.println("enter the number");
+    n=sc.nextInt();
+    for(i=1;i<=n;i++)
+    {
+      
+      sum=sum+i;
+      }
+      System.out.println("The sum :"+sum);
+      }
+      }
